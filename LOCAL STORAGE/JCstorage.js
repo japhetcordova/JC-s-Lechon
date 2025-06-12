aa1452efe1022034cf2edfc84a05e71160655243
vrax7 <vraxx07@gmail.com>
2025-06-12 15:43:33 +0800

    made graph samples that will be used for the final output

--- LOCAL STORAGE/JCstorage.js
+++ LOCAL STORAGE/JCstorage.js
@@ -62,9 +62,57 @@ const barGraph = new Chart("BarGraph", {
             yAxes: [{
                 ticks: {beginAtZero: true},
             }]
-        }
+        },
+        title: {
+        display: true,
+        text: "Bar Graph Sample"
+    }
+    },
+    
+})
+
+const pieGraph = new Chart("PieGraph", {
+    type: "pie",
+    data: {
+        labels: xAxis,
+        datasets: [{
+            backgroundColor: barColor,
+            data: yAxis
+        }]
+    },
+    options: {
+        title: {
+                display: true,
+                text: "Pie Graph Sample"
+            }
+    },
+})
+
+
+
+const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+
+const lineGraph = new Chart("LineGraph", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 6, max:16}}],
     },
     title: {
-
+        display: true,
+        text: "Line Graph Sample"
     }
-})
+  }
+});
